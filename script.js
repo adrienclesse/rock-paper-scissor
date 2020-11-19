b3a95c373270fb30b66eab36361bda3967e60e24
adrienclesse <adrienclesse@hotmail.com>
2020-11-19 14:54:08 +0100

    finish

--- script.js
+++ script.js
@@ -17,22 +17,25 @@
     function comparaison(userChoice){
     
         if (userChoice===computerChoice) {
-            document.getElementById("draw").innerHTML="it's a draw";
+            document.getElementById("draw").innerHTML="it's a draw, please presse the Play again Button";
         }
         else if (userChoice==="rock") {
-            if(computerChoice==="scissor") {document.getElementById("info").innerHTML="You won!, Your oponent had a scissor";}
-            else if(computerChoice==="paper") {document.getElementById("losing").innerHTML="You've lost, unfortunately, against paper";}
+            if(computerChoice==="scissor") {document.getElementById("info").innerHTML="You won! Your oponent had a scissor, please presse the Play again Button";}
+            else if(computerChoice==="paper") {document.getElementById("losing").innerHTML="You've lost, unfortunately, against paper, please presse the Play again Button";}
         }
 
         else if (userChoice==="paper") {
-            if(computerChoice==="rock") {document.getElementById("info").innerHTML="You won!, paper beats rock";}
-            else if(computerChoice==="scissor") {document.getElementById("losing").innerHTML="You've lost, against a cutting scissor";}
+            if(computerChoice==="rock") {document.getElementById("info").innerHTML="You won! The paper beats the rock, please presse the Play again Button";}
+            else if(computerChoice==="scissor") {document.getElementById("losing").innerHTML="You've lost, against a cutting scissor, please presse the Play again Button";}
         }
 
         else if (userChoice==="scissor") {
-            if(computerChoice==="paper") {document.getElementById("info").innerHTML="You won!, scissor cuts the paper";}
-            else if(computerChoice==="rock") {document.getElementById("losing").innerHTML="You've lost, broken by a rock";}
+            if(computerChoice==="paper") {document.getElementById("info").innerHTML="You won! The scissor cuts the paper, please presse the Play again Button";}
+            else if(computerChoice==="rock") {document.getElementById("losing").innerHTML="You've lost, broken by a rock, please presse the Play again Button";}
         }
+        function disabled() {
+            document.getElementById("myBtn").disabled = true;
+          }
    
     };
     
@@ -46,6 +49,7 @@
         const userChoice="paper";
         console.log(userChoice);
         comparaison(userChoice);
+
     });
 
     document.getElementById("scissor").addEventListener("click",function () {
